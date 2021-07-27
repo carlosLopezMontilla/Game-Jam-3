8190d807cbc0a32e8a0810e88dfd6d83e63a0579
CLopezMontilla <77298181+carlosLopezMontilla@users.noreply.github.com>
2021-07-27 13:05:04 +0200

    3th commit
    Movimiento del personaje

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
+
+    public float speed;
+    private Rigidbody _rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        _rb.velocity = new Vector3(x * speed, _rb.velocity.y, z * speed);
     }
 }
